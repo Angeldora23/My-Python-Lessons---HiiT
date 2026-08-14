0641c317a3379a8b0209f93b24ce5cd473a4c970
Etim Dorathy <doraetim23@gmail.com>
2026-08-14 12:18:25 +0100

    create user form and custom user creation form

--- Django_setup/projectapp/urls.py
+++ Django_setup/projectapp/urls.py
@@ -6,11 +6,10 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("my_profile/", views.my_profile, name="my_profile"),
     path("posts/", views.posts, name="posts"),
+    path("posts/add/", views.add_post, name="add_post"),
     path("post/<str:pk>/", views.post, name="post"),
 
-# this is for the user form created for the user form
     path("user_form", views.display_form, name="user_form"),
+    path("user_custom_create", views.custom_create_user, name="user_custom_create"),
     path("user_submit", views.submit_form, name="submit_form"),
-    ]
-
-
+]
--- Django_setup/projectapp/views.py
+++ Django_setup/projectapp/views.py
@@ -1,7 +1,10 @@
+from django.core.checks import messages
+from django.core.mail import message
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
+from projectapp.forms import PostForm
 from projectapp.models import Post
-
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "index.html")
@@ -61,3 +64,40 @@ def submit_form(request):
 
     return redirect("user_form")
 
+def add_post(request):
+    form = PostForm()
+
+    context = {"post_form": form}
+    return render(request, "post_form.html", context)
+
+#THIS IS DEFINING THE FUNCTION FOR CREATING USERS WITH THEIR LOGINS
+# WE CREATED USING OUR OWN FORMS AND NOT PYTHON FORMS
+
+def custom_create_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if not (username and email and password and confirm_password):
+            messages.error(request, "All fields are required.")
+            return redirect("user_custom_create")
+
+        if User.objects.filter(username__iexact=username).exists():
+            messages.error(request, "Username taken.")
+            return redirect("user_custom_create")
+
+        if User.objects.filter(email__iexact=email).exists():
+            messages.error(request, "Email already exists.")
+            return redirect("user_custom_create")
+
+        if password != confirm_password:
+            messages.error(request, "Passwords must match.")
+            return redirect("user_custom_create")
+
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, "Account created successfully.")
+        return redirect("home")
+
+    return render(request, "create_user.html")
